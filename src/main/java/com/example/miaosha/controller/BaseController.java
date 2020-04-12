c153500d61e87a96f7b8de525736d066b41b21c3
zbf <hgkdzbf6@qq.com>
2020-04-12 21:19:08 +0800

    3-8 用户模型管理--getotp页面实现

--- src/main/java/com/example/miaosha/controller/BaseController.java
+++ src/main/java/com/example/miaosha/controller/BaseController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class BaseController {
 
+    public static final String CONTENT_TYPE_FORMED = "application/x-www-form-urlencoded";
+
     // 定义exceptionHandler，解决未被controller层吸收的exception
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.OK)
--- src/main/java/com/example/miaosha/controller/UserController.java
+++ src/main/java/com/example/miaosha/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.example.miaosha.controller;
 
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.example.miaosha.controller.vo.UserVO;
 import com.example.miaosha.error.BusinessException;
 import com.example.miaosha.error.EmBusinessError;
@@ -7,19 +11,29 @@ import com.example.miaosha.response.CommonReturnType;
 import com.example.miaosha.service.UserService;
 import com.example.miaosha.service.model.UserModel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("user")
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController extends BaseController{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BaseController.class);
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
 
     @RequestMapping("/get")
     @ResponseBody
@@ -32,6 +46,24 @@ public class UserController extends BaseController{
         // 将核心领域模型用户对象，转化为可供UI使用的VO
         UserVO userVO = convertFromModel(userModel);
         return CommonReturnType.create(userVO);
+    }
+
+    // 用户获取otp短信接口
+    @RequestMapping(method={RequestMethod.POST}, value = "/getotp",consumes = {BaseController.CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "phone")String phone){
+        // 按照一定的规则生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+        // 将OTP验证码与对应用户的手机号关联，一般使用redis使用。
+        // 目前还是http session的方式。
+        httpServletRequest.getSession().setAttribute(phone, otpCode);
+        // 将OTP验证码通过短信通道，发送给用户，省略
+        LOGGER.info("phone = " +phone+"& otpCode = " +otpCode);
+        System.out.println("phone = " +phone+"& otpCode = " +otpCode);
+        return CommonReturnType.create(null);
     }
 
     private UserVO convertFromModel(UserModel userModel){
